4f6b09d81d0cd134bf54cfd7ee970399a9bb3770
p <p@ps-MacBook-Air.local>
2016-08-09 17:13:46 -0500

    added ngroute and routes

--- angular.js
+++ angular.js
@@ -1,5 +1,26 @@
-var app = angular.module('instacritic', []);
+var app = angular.module('instacritic', ['ngRoute']);
 app.controller('instaCtrl', function($scope) {
   $scope.add = function() {
   }
 });
+
+module.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+      .when('/', {
+          template: 'This is the default route',
+          templateUrl: 'view/index.html',
+          controller: 'instaCtrl'
+      }).when('/users', {
+          template: 'This is the users route',
+          templateUrl: 'view/templates/users.html',
+          controller: 'instaReview'
+      }).when('/shows', {
+          template: 'This is the shows route',
+          templateUrl: 'view/templates/shows.html',
+          controller: 'instaShows'
+      }).when('/reviews', {
+          template: 'This is the reviews route',
+          templateUrl: 'view/templates/review.html',
+          controller: 'instaReviews'
+      });
+}]);
