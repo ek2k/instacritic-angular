05d264d8b7327880d6880428cf6c0cb24a85c295
p <p@ps-MacBook-Air.local>
2016-08-09 15:33:06 -0500

    working on adding data to the database

--- angular.js
+++ angular.js
@@ -1,9 +1,27 @@
 var app = angular.module('instacritic', ['firebase']);
-app.controller('instaCtrl', function($scope, $firebaseOject) {
+app.controller('instaCtrl', function($scope, $firebaseArray, $firebaseAuth) {
   var ref = new Firebase("https://instacritic.firebaseio.com/");
 
-  var syncObject = $firebaseObject(ref);
+  $scope.criticArray = $firebaseArray(ref);
 
-  syncObject.$bindTo($scope, "data");
+  $scope.critics = $firebaseArray(ref);
+
+  $scope.addReview = function() {
+    $scope.critics.$add({
+      show: $scope.show,
+      genre: $scope.genre,
+
+    })
+  }
+
+  var auth = $firebaseAuth(ref);
+
+  auth.$authWithOutAuthPopup("facebook").then(function(authData) {
+    console.log("Logged in as:", authData.uid);
+  }).catch(function(error) {
+    console.log("Authenticaiton failed:", error);
+  });
+
+  // syncObject.$bindTo($scope, "data");
   //can use data.name etc fo render data in the dom
 });
