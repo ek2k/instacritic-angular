386d8e761d1b37b75ac2e3ebe8f0d8c5988ccdfa
p <p@ps-MacBook-Air.local>
2016-08-10 12:38:21 -0500

    finished database

--- seeds/populate_users.js
+++ seeds/populate_users.js
@@ -5,6 +5,7 @@ exports.seed = function(knex, Promise) {
             return Promise.all([
                 // Inserts seed entries
                 knex('users').insert({
+                    id: 1,
                     username: 'pmoney',
                     password: 'password',
                     email: 'price@price.com',
@@ -13,6 +14,7 @@ exports.seed = function(knex, Promise) {
                     state: 'PA'
                 }),
                 knex('users').insert({
+                    id: 2,
                     username: 'ek2k',
                     password: 'password',
                     email: 'ed@ededneddy.com',
@@ -21,6 +23,7 @@ exports.seed = function(knex, Promise) {
                     state: 'CO'
                 }),
                 knex('users').insert({
+                    id: 3,
                     username: 'partyboob',
                     password: 'password',
                     email: 'kbleezey@karen.com',
@@ -34,16 +37,19 @@ exports.seed = function(knex, Promise) {
         }).then(function() {
             return Promise.all([
                 knex('shows').insert({
+                    id: 25,
                     name: 'Mr. Robot',
                     network: 'Usa',
                     genre: 'Drama'
                 }),
                 knex('shows').insert({
+                    id: 26,
                     name: 'Stranger Things',
                     network: 'Netflix',
                     genre: 'Sci-Fi'
                 }),
                 knex('shows').insert({
+                    id: 27,
                     name: 'Rick and Morty',
                     network: 'Cartoon network',
                     genre: 'Animated Sitcom'
@@ -53,37 +59,43 @@ exports.seed = function(knex, Promise) {
             }).then(function() {
                 return Promise.all([
                     knex('episodes').insert({
-                        show_id: 24,
+                        id: 1,
+                        show_id: 25,
                         episode_number: 2,
                         episode_name: 'Chapter 2: The Weirdo on Maple Street',
                         season_number: 1
                     }),
                     knex('episodes').insert({
-                        show_id: 24,
+                        id: 2,
+                        show_id: 25,
                         episode_number: 5,
                         episode_name: 'Chapter 5: The Flea and the Acrobat',
                         season_number: 1
                     }),
                     knex('episodes').insert({
-                        show_id: 23,
+                        id: 3,
+                        show_id: 26,
                         episode_number: 2,
                         episode_name: 'Mortynight Run',
                         season_number: 2
                     }),
                     knex('episodes').insert({
-                        show_id: 23,
+                        id: 4,
+                        show_id: 26,
                         episode_number: 2,
                         episode_name: 'Lawnmower Dog',
                         season_number: 1
                     }),
                     knex('episodes').insert({
-                        show_id: 22,
+                        id: 5,
+                        show_id: 27,
                         episode_number: 1,
                         episode_name: '0hellofriend.mov',
                         season_number: 1
                     }),
                     knex('episodes').insert({
-                        show_id: 22,
+                        id: 6,
+                        show_id: 27,
                         episode_number: 3,
                         episode_name: '1k3rnel-pan1c.ksd',
                         season_number: 2
@@ -94,29 +106,30 @@ exports.seed = function(knex, Promise) {
                     return Promise.all([
                         knex('user_shows').insert({
                             user_id: 2,
-                            show_id: 3
+                            show_id: 27
                         }),
                         knex('user_shows').insert({
                             user_id: 1,
-                            show_id: 2
+                            show_id: 26
                         }),
                         knex('user_shows').insert({
                             user_id: 3,
-                            show_id: 1
+                            show_id: 25
                         }),
                         knex('user_shows').insert({
                             user_id: 2,
-                            show_id: 1
+                            show_id: 25
                         }),
                         knex('user_shows').insert({
                             user_id: 1,
-                            show_id: 2
+                            show_id: 26
                         })
                     ]).then(function() {
                         return knex('user_reviews').del()
                     }).then(function() {
                         return Promise.all([
                             knex('user_reviews').insert({
+                                id: 1,
                                 user_id: 2,
                                 episode_id: 6,
                                 review: 'I like mr robot episode 3',
@@ -124,6 +137,7 @@ exports.seed = function(knex, Promise) {
                                 star_rating: 5
                             }),
                             knex('user_reviews').insert({
+                                id: 2,
                                 user_id: 1,
                                 episode_id: 1,
                                 review: 'I like stranger things episode 2',
@@ -131,6 +145,7 @@ exports.seed = function(knex, Promise) {
                                 star_rating: 5
                             }),
                             knex('user_reviews').insert({
+                                id: 3,
                                 user_id: 3,
                                 episode_id: 4,
                                 review: 'Dogs take over the world then escape',
@@ -138,6 +153,7 @@ exports.seed = function(knex, Promise) {
                                 star_rating: 4
                             }),
                             knex('user_reviews').insert({
+                                id: 4,
                                 user_id: 3,
                                 episode_id: 3,
                                 review: 'I like rick and morty episode 2',
