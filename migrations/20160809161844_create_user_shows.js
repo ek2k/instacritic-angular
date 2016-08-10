975717a2b5a343c1d5d66b3dd3f7c2e54c8d0836
Ed <ek2000@gmail.com>
2016-08-10 13:38:45 -0500

    fixes migration table

--- migrations/20160809161844_create_user_shows.js
+++ migrations/20160809161844_create_user_shows.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('user_shows', (table) => {
     table.increments();
     table.integer('user_id').unsigned().index().references('id').inTable('users').onDelete('cascade');
-    table.integer('showp_id').unsigned().index().references('id').inTable('shows').onDelete('cascade');
+    table.integer('show_id').unsigned().index().references('id').inTable('shows').onDelete('cascade');
   })
 };
 
