0ba0b0a2e0d06c9f3b7628facb724271a84e3ead
p <p@ps-MacBook-Air.local>
2016-08-11 15:23:12 -0500

    added the about section

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,49 +2,6 @@ var app = angular.module('instacritic', ['ngRoute']);
 
 console.log('booyah');
 
-
-<<<<<<< HEAD
-=======
-app.config(function($routeProvider) {
-  $routeProvider
-      .when('/', {
-          templateUrl: 'view/templates/home.html',
-          controller: 'IndexController',
-          controllerAs: 'index'
-      })
-      .when('/users', {
-          templateUrl: 'view/templates/users.html',
-          controller: 'ReviewController',
-          controllerAs: 'users'
-      })
-      .when('/users/new', {
-          templateUrl: 'view/templates/newUser.html',
-          controller: 'UserController',
-          controllerAs: 'users'
-      })
-      .when('/users/:id/edit', {
-          templateUrl: 'view/templates/editUser.html',
-          controller: 'UserController',
-          controllerAs: 'users'
-      })
-      .when('/users/:id/delete', {
-          templateUrl: 'view/templates/delUser.html',
-          controller: 'UserController',
-          controllerAs: 'users'
-      })
-      .when('/shows', {
-          templateUrl: 'view/templates/shows.html',
-          controller: 'ShowController',
-          controllerAs: 'shows'
-      })
-      .when('/reviews', {
-          templateUrl: 'view/templates/books.html',
-          controller: 'ReviewController',
-          controllerAs: 'reviews'
-      });
-});
-
->>>>>>> d4681c669d9faf06d12fdb26c75f839da264e233
 app.config(function($routeProvider, $locationProvider) {
   $routeProvider
       .when('/', {
@@ -71,6 +28,10 @@ app.config(function($routeProvider, $locationProvider) {
           templateUrl: 'view/templates/books.html',
           controller: 'ReviewController',
           controllerAs: 'reviews'
+      })
+      .when('/about', {
+        templateUrl: 'view/templates/about.html',
+        controller: 'AboutController',
       });
 });
 
@@ -83,7 +44,7 @@ app.controller('IndexController', ['$scope', '$routeParams', '$location', '$http
 
   $scope.NewUser = function(newUser) {
     console.log("clicked");
-    newData = angular.copy(user);
+    newData = angular.copy(newUser);
     console.log(newData);
     $http({
       method: 'POST',
