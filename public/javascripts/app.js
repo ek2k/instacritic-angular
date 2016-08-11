3141f366622846e8aa7471d4ae2682b44c967a12
karen818 <klc0818@outlook.com>
2016-08-11 13:56:13 -0500

    adds ajax call to review page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,44 +2,44 @@ var app = angular.module('instacritic', ['ngRoute']);
 
 console.log('booyah');
 
-app.config(function($routeProvider) {
-  $routeProvider
-      .when('/', {
-          templateUrl: 'view/templates/home.html',
-          controller: 'IndexController',
-          controllerAs: 'index'
-      })
-      .when('/users', {
-          templateUrl: 'view/templates/users.html',
-          controller: 'UserController',
-          controllerAs: 'users'
-      })
-      .when('/users/new', {
-          templateUrl: 'view/templates/newUser.html',
-          controller: 'UserController',
-          controllerAs: 'users'
-      })
-      .when('/users/:id/edit', {
-          templateUrl: 'view/templates/editUser.html',
-          controller: 'UserController',
-          controllerAs: 'users'
-      })
-      .when('/users/:id/delete', {
-          templateUrl: 'view/templates/delUser.html',
-          controller: 'UserController',
-          controllerAs: 'users'
-      })
-      .when('/shows', {
-          templateUrl: 'view/templates/shows.html',
-          controller: 'ShowController',
-          controllerAs: 'shows'
-      })
-      .when('/reviews', {
-          templateUrl: 'view/templates/books.html',
-          controller: 'ReviewController',
-          controllerAs: 'reviews'
-      });
-});
+// app.config(function($routeProvider) {
+//   $routeProvider
+//       .when('/', {
+//           templateUrl: 'view/templates/home.html',
+//           controller: 'IndexController',
+//           controllerAs: 'index'
+//       })
+//       .when('/users', {
+//           templateUrl: 'view/templates/users.html',
+//           controller: 'TVController',
+//           controllerAs: 'users'
+//       })
+//       .when('/users/new', {
+//           templateUrl: 'view/templates/newUser.html',
+//           controller: 'UserController',
+//           controllerAs: 'users'
+//       })
+//       .when('/users/:id/edit', {
+//           templateUrl: 'view/templates/editUser.html',
+//           controller: 'UserController',
+//           controllerAs: 'users'
+//       })
+//       .when('/users/:id/delete', {
+//           templateUrl: 'view/templates/delUser.html',
+//           controller: 'UserController',
+//           controllerAs: 'users'
+//       })
+//       .when('/shows', {
+//           templateUrl: 'view/templates/shows.html',
+//           controller: 'ShowController',
+//           controllerAs: 'shows'
+//       })
+//       .when('/reviews', {
+//           templateUrl: 'view/templates/books.html',
+//           controller: 'ReviewController',
+//           controllerAs: 'reviews'
+//       });
+// });
 
 
 app.controller('IndexController', function($scope) {
@@ -69,18 +69,25 @@ app.controller('UserController', ['$scope', '$routeParams', '$http', function($s
   })
 }])
 
-  $http.put('/users/:id/edit', updateData).then(function(users){
-    $scope.view = users;
-  })
-
-  $http.delete('/users/:id/delete', Data)
 
 app.controller('UserController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
   $scope.view = {};
   $http ({})
-}])
+}]);
 
-app.controller('UserController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+
+app.controller("ReviewController", function($scope, $http) {
   $scope.view = {};
-  $http ({})
-}])
+  $scope.view.findShow = function(){
+      var title = $scope.view.title;
+      $http ({
+        method: 'GET',
+        url: 'http://api.tvmaze.com/singlesearch/shows?q=' + title
+    }).then(function successCallback(response){
+        $scope.view.showTitle = response.data.name;
+        $scope.view.showImg = response.data.image.medium;
+        console.log(response.data.image.medium);
+        console.log(response);
+    });
+  }
+});
